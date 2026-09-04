64f29c8f0e430747a783b95a4b86685d2846af22
Nqtural <robin.stalstad@proton.me>
2026-09-05 01:59:38 +0200

    feat(backend): add production/development setting
    `cargo run` => development
    `cargo run --release` => production

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -6,14 +6,40 @@ mod routes;
 mod storage;
 mod types;
 
+#[allow(unused)] // linter runs with debug assertions
+enum Mode {
+	Development,
+	Production,
+}
+
 #[tokio::main]
 async fn main() {
-	dotenvy::dotenv().ok();
+	#[cfg(not(debug_assertions))]
+	let mode = Mode::Production;
+	#[cfg(debug_assertions)]
+	let mode = Mode::Development;
 
-	let bind_address = std::env::var("BIND_ADDRESS").unwrap_or("0.0.0.0:3000".to_string());
-	let frontend_url = std::env::var("FRONTEND_URL").unwrap_or("http://localhost:5173".to_string());
-	let database_url = std::env::var("DATABASE_URL").unwrap_or("sqlite://app.db".to_string());
+	let (bind_address, frontend_url, database_url) = if matches!(mode, Mode::Development) {
+		println!("info: Running in development mode");
+		(
+			"0.0.0.0:3000".to_string(),
+			"http://localhost:5173".to_string(),
+			"sqlite://app.db".to_string(),
+		)
+	} else {
+		dotenvy::dotenv().ok();
+		(
+			std::env::var("BIND_ADDRESS")
+				.expect("fatal: BIND_ADDRESS environment variable not set"),
+			std::env::var("FRONTEND_URL")
+				.expect("fatal: FRONTEND_URL environment variable not set"),
+			std::env::var("DATABASE_URL")
+				.expect("fatal: DATABASE_URL environment variable not set"),
+		)
+	};
 
+	// not needed if backend and frontend are hosted on
+	// the same origin, kept for people that may want it
 	let cors = CorsLayer::new()
 		.allow_origin(frontend_url.parse::<axum::http::HeaderValue>().unwrap())
 		.allow_methods(Any)
